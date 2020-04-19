bd42580d519d58783804d2fd7aa4c51415c2c82f
Adriano <developer.adrianoscjr@gmail.com>
2020-04-19 18:14:51 -0300

    Alertas do tipo LongClick, BottomSheet e Share

--- lib/pages/carro/carros_listview.dart
+++ lib/pages/carro/carros_listview.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carros/pages/carro/carro_page.dart';
 import 'package:carros/utils/nav.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 import 'carro.dart';
 
@@ -19,51 +20,62 @@ class CarrosListView extends StatelessWidget {
         itemBuilder: (context, index) {
           Carro c = carros[index];
 
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: CachedNetworkImage(
-                      imageUrl: c.urlFoto ??
-                          "http://www.livroandroid.com.br/livro/carros/esportivos/BMW.png",
-                      width: 150,
-                    ),
-                  ),
-                  Text(
-                    c.nome ?? "Nome do carro",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    c.descricao,
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  ButtonBarTheme(
-                    data: ButtonBarTheme.of(context),
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(
-                            child: const Text('DETALHES'),
-                            onPressed: () => _onClickCarro(context, c)),
-                        FlatButton(
-                          child: const Text('SHARE'),
-                          onPressed: () {
-                            /* ... */
-                          },
+          return Container(
+            height: 280,
+            child: InkWell(
+              onTap: () {
+                _onClickCarro(context, c);
+              },
+              onLongPress: () {
+                _onLongClickCarro(context, c);
+              },
+              child: Card(
+                color: Colors.grey[100],
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                        child: CachedNetworkImage(
+                          imageUrl: c.urlFoto ??
+                              "http://www.livroandroid.com.br/livro/carros/esportivos/BMW.png",
+                          width: 150,
                         ),
-                      ],
-                    ),
+                      ),
+                      Text(
+                        c.nome ?? "Nome do carro",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                      Text(
+                        c.descricao,
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      ButtonBarTheme(
+                        data: ButtonBarTheme.of(context),
+                        child: ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                                child: const Text('DETALHES'),
+                                onPressed: () => _onClickCarro(context, c)),
+                            FlatButton(
+                              child: const Text('SHARE'),
+                              onPressed: () {
+                                _onClickShare(context, c);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           );
@@ -74,5 +86,48 @@ class CarrosListView extends StatelessWidget {
 
   _onClickCarro(context, Carro c) {
     push(context, CarroPage(c));
+  }
+
+  _onLongClickCarro(BuildContext context, Carro c) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  c.nome,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text("Detalhes"),
+                leading: Icon(Icons.directions_car),
+                onTap: () {
+                  pop(context);
+                  _onClickCarro(context, c);
+                },
+              ),
+              ListTile(
+                title: Text("Share"),
+                leading: Icon(Icons.share),
+                onTap: () {
+                  pop(context);
+                  _onClickShare(context, c);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _onClickShare(BuildContext context, Carro c) {
+
+    Share.share(c.urlFoto);
   }
 }
